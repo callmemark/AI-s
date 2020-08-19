06042424bdfb378ad21b3259152c7b3b31db578c
Mark John A. Velmonte <65215676+xxmeowxx@users.noreply.github.com>
2020-08-19 19:00:45 +0800

    improve error and correct handling
    It is now able to remember its correct answers

--- Color_Ai/Learner_Color_Ai/ColorAI.py
+++ Color_Ai/Learner_Color_Ai/ColorAI.py
@@ -1,64 +1,49 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
+import numpy as np
 
+from sklearn.neighbors import KNeighborsClassifier
 
-def getColor(color_inp, data_ref):
-    data = data_ref
-    
-    
-    R = data["R"]
-    G = data["G"]
-    B = data["B"]
-
-    X = data.iloc[:, :-2].values
-    y = data["Id"]
-    
-    
-    model = KNeighborsClassifier(n_neighbors = 14)
-    model.fit(X, y)
-
-
-    u_input = color_inp
-
-
-    prediction = model.predict([u_input])
-    #print(prediction)
-
-
-    if prediction == [1]: print("red-shade")
-    if prediction == [2]: print("orange-shade")
-    if prediction == [3]: print("yellow-shade")
-    if prediction == [4]: print("yellowgreen-shade")
-    if prediction == [5]: print("green-shade")
-    if prediction == [6]: print("bluegreen-shade")
-    if prediction == [7]: print("neon-shade")
-    if prediction == [8]: print("lightblue-shade")
-    if prediction == [9]: print("blue-shade")
-    if prediction == [10]: print("blueviolet-shade")
-    if prediction == [11]: print("violet-shade")
-    if prediction == [12]: print("pink-shade")
-    if prediction == [13]: print("black-shade")
-    if prediction == [14]: print("white-shade")
-
-
-
-
-class ColorAI():
+class Learn():
     def __init__(self):
         self.trained_data = pd.read_csv("learned_color_data.csv")
+    
+
+    
+    def getColor(self, color_inp, data_ref):
+        self.data = data_ref
 
 
-    def help(self):
-        print("showDataFrameInfo, showQuizScore, quiz, showExamScore, teach, exam")
+        R = self.data["R"]
+        G = self.data["G"]
+        B = self.data["B"]
+
+        X = self.data.iloc[:, :-2].values
+        y = self.data["Id"]
+
+
+        model = KNeighborsClassifier(n_neighbors = 27)
+        model.fit(X, y)
+
+
+        u_input = color_inp
+
+
+        prediction = model.predict([u_input])
+
+        self.prediction_index = np.where(self.data == prediction[0])[0][0]
+        
+        print("prediction:", self.data["Color name"][self.prediction_index])
+        print("prediction_index:", self.data["Id"][self.prediction_index])
+  
     
     
-    def showDataFrameInfo(self):
-        print(self.trained_data.shape)
+    def showDataFrame(self):
+        print(self.trained_data)
     
     
     def quiz(self):
-        n_test = 10
+        n_test = 5
         n_correct = 0
         
         while n_test != 0:
@@ -72,7 +57,7 @@ class ColorAI():
             
             RGB = [R, G, B]
             
-            getColor(RGB, self.trained_data)
+            self.getColor(RGB, self.trained_data)
 
             answer_status = input("answer status :")
 
@@ -95,20 +80,7 @@ class ColorAI():
         new_score_data.to_csv("Scores.csv")
     
     
-    def showQuizScore(self):
-        scores = score_data = pd.read_csv("Scores.csv")
-        print(scores["Scores"])
-
-        plt.title("AI Scores")
-        score_point = scores["Scores"]
-        plt.plot(score_point, color = "green")
-        plt.plot(score_point, "o",color = "orange")
-        plt.xlabel("test count")
-        plt.ylabel("score")
-        plt.show()
-
-
-
+    
     def exam(self):
         given = pd.read_csv(input("exam sheet:"))
         
@@ -120,7 +92,7 @@ class ColorAI():
             for num in l_quest:
                 uinp.append(int(num))
                 
-            getColor(uinp, self.trained_data)
+            self.getColor(uinp, self.trained_data)
         
         print(given["answer"])
         
@@ -136,13 +108,20 @@ class ColorAI():
     
     def showExamScore(self):
         scores = pd.read_csv("exam_scores.csv")
-        print(scores["score"])
-
-
-        plt.plot(scores["score"])
+        print(scores)
+        
+        plt.plot(scores["score"], "o", color = "orange")
+        plt.plot(scores["score"], color = "green")
         plt.show()
-
-
+        
+    
+    
+    def showQuizScore(self):
+        scores = score_data = pd.read_csv("Scores.csv")
+        print(scores)
+        
+        plt.plot(scores["Scores"])
+        plt.show()
             
     
     def teach(self):
@@ -164,12 +143,21 @@ class ColorAI():
 
             RGB = [R, G, B]
 
-            getColor(RGB, self.trained_data)
+            self.getColor(RGB, self.trained_data)
 
             answer_status = input("answer status C/W:")
 
             if answer_status == "C":
                 n_correct += 1
+                
+                shade_fam = self.data["Color name"][self.prediction_index]
+                data_id = self.data["Id"][self.prediction_index]
+                new_data = pd.DataFrame({"R":R, "G":G, "B":B, "Color name":shade_fam, "Id":data_id}, index = [0])
+                
+                self.trained_data = pd.concat([new_data, self.trained_data]).reset_index(drop = True)
+                self.trained_data.to_csv("learned_color_data.csv", index=False)
+                
+                print(new_data)
 
             if answer_status == "W":
                 n_wrong += 1
@@ -195,8 +183,6 @@ class ColorAI():
                 break
 
 
-
-guide = pd.read_csv("learned_color_data_2.csv")
-pd.set_option("display.max_rows", 200)
-print(guide)
-
+guide = pd.read_csv("learned_color_data.csv")
+pd.set_option("display.max_rows", 500)
+print(guide.shape)
